9e65688814a8310df0be2f175441d62462022f86
Kesomannen <bobbo.nidor@gmail.com>
2024-07-30 19:23:52 +0200

    back to poppins gang

--- src/routes/gale/+page.svelte
+++ src/routes/gale/+page.svelte
@@ -31,7 +31,7 @@
 
 <div class="flex flex-col w-full max-w-[65rem] mx-auto items-center py-20 px-8 text-white relative">
 	<div class="text-center">
-		<h1 class="md:text-7xl text-5xl font-extrabold">The mod manager you've been waiting for</h1>
+		<h1 class="md:text-7xl text-5xl font-bold">The mod manager you've been waiting for</h1>
 		<h3 class="text-2xl pt-4 text-slate-300">
 			Gale is a lightweight and modern mod manager for Thunderstore
 		</h3>
@@ -39,7 +39,7 @@
 
 	<div class="button-container flex items-center py-6 text-xl gap-2">
 		<button
-			class="flex items-center gap-2 text-white bg-blue-600 hover:bg-blue-500 px-6 py-2 rounded-xl font-semibold shadow-xl"
+			class="flex items-center gap-2 text-white bg-blue-600 hover:bg-blue-500 px-6 py-2 rounded-xl font-medium shadow-xl"
 			onclick={() => {
 				downloadsHeader.scrollIntoView({ behavior: 'smooth', block: 'start' });
 			}}><Download />Download</button
@@ -59,7 +59,7 @@
 	</div>
 
 	<div class="py-12">
-		<h3 class="text-4xl font-bold text-center">Features</h3>
+		<h3 class="text-4xl font-semibold text-center">Features</h3>
 		<div class="text-center text-lg pt-2 text-slate-400">
 			Gale does everything you'd expect from a mod manager, and more:
 		</div>
@@ -68,7 +68,7 @@
 			<div class="flex items-center gap-8">
 				<Speed class="size-12 flex-shrink-0" />
 				<div class="text-slate-300 text-lg flex-shrink text-wrap">
-					<div class="text-2xl font-semibold text-white">Fast & lightweight</div>
+					<div class="text-2xl font-medium text-white">Fast & lightweight</div>
 					Built with Rust and SvelteKit, Gale is extremely performant and responsive, while also using
 					less resources than other mod managers.
 				</div>
@@ -77,7 +77,7 @@
 			<div class="flex items-center gap-8">
 				<Modpack class="size-12 flex-shrink-0" />
 				<div class="text-slate-300 text-lg flex-shrink text-wrap">
-					<div class="text-2xl font-semibold text-white">Modpack creator</div>
+					<div class="text-2xl font-medium text-white">Modpack creator</div>
 					Export your profiles as modpacks and publish them to Thunderstore from within the app. Customize
 					the readme, changelog, included files and more.
 				</div>
@@ -86,7 +86,7 @@
 			<div class="flex items-center gap-8">
 				<Config class="size-12 flex-shrink-0" />
 				<div class="text-slate-300 text-lg flex-shrink text-wrap">
-					<div class="text-2xl font-semibold text-white">Config editor</div>
+					<div class="text-2xl font-medium text-white">Config editor</div>
 					Configure mods through a feature-rich and easy-to-use editor. Options are beautifully displayed
 					as dropdowns, sliders, lists and more.
 				</div>
@@ -95,7 +95,7 @@
 	</div>
 
 	<div class="py-12 text-center w-full max-w-[45rem]">
-		<h3 class="text-4xl font-bold" bind:this={downloadsHeader}>Downloads</h3>
+		<h3 class="text-4xl font-semibold" bind:this={downloadsHeader}>Downloads</h3>
 
 		<div class="flex flex-col pt-4 gap-3 w-full text-xl">
 			<div class="flex items-center gap-3">
