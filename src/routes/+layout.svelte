e44b3b9b3f7597017b39d467703e11b57dd42c14
Kesomannen <bobbo.nidor@gmail.com>
2025-05-13 10:23:48 +0200

    add yatzy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,3 @@
 <div class="overflow-x-hidden">
 	<slot />
 </div>
-
-<style lang="postcss">
-	:global(html) {
-		background: linear-gradient(180deg, theme(colors.gray.900) 0%, theme(colors.gray.800) 100%);
-	}
-</style>
---
+++ src/routes/yatzy/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import Button from '$lib/components/Button.svelte';
+	import Die from '$lib/components/Die.svelte';
+	import PlayerCard from '$lib/components/PlayerCard.svelte';
+	import { game } from '$lib/game.svelte';
+</script>
+
+<main
+	class="mx-auto h-full flex w-full flex-col items-center justify-center gap-12 overflow-y-auto p-6 lg:flex-row"
+>
+	<div class="flex flex-col items-center">
+		<h1
+			class="font-display bg-gradient-to-tr from-amber-600 to-amber-700 bg-clip-text pb-4 text-8xl text-transparent"
+		>
+			Yatzy
+		</h1>
+
+		<div class="flex items-center gap-1">
+			{#each game.dice as die}
+				<Die {die} />
+			{/each}
+		</div>
+
+		<Button
+			size="lg"
+			icon="tabler:dice-2-filled"
+			onclick={() => game.rollDice()}
+			disabled={game.rollsLeft <= 0}
+			class="my-2"
+		>
+			Kasta ({game.rollsLeft} kvar)
+		</Button>
+	</div>
+
+	<div class="flex w-full max-w-lg gap-8">
+		{#each game.players as player}
+			<PlayerCard {player} />
+		{/each}
+	</div>
+</main>
+
+<style>
+	main {
+		background-color: white;
+	}
+</style>
