3bbe7fb2a7c5b5b7205f8e5ceb308664c1cec7bf
Bo Rodin <bobbo.nidor@gmail.com>
2026-06-18 09:44:33 +0200

    Nuke website

--- src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-</script>
-
-<div class="w-full h-screen flex items-center justify-center">
-	<a href="/gale" class="text-xl hover:underline text-green-500 hover:text-green-400">Gale</a>
-</div>
